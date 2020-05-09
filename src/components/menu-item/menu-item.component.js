5d0b1812fb223598424f3186003d4757621b4b48
Marvin Cespedes <marvin.cespedes@benchmarkanalytics.com>
2020-05-09 07:54:48 -0500

    styling menu item

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -8,9 +8,15 @@ const MenuItem = ({ title, imageUrl, size }) => (
     }}
     className={`${size} menu-item`}
   >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    ></div>
     <div className="content">
       <h1 className="title">{title}</h1>
-      <spam className="subtitle">Shop Now</spam>
+      <span className="subtitle">Shop Now</span>
     </div>
   </div>
 );
